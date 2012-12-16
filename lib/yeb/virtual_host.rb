734211363e71a5448dfff5b58f88752f77908ea9
Marcin Kulik <marcin.kulik@gmail.com>
2012-12-16 21:37:36 +0100

    VirtualHost

---
+++ lib/yeb/virtual_host.rb
@@ -0,0 +1,83 @@
+require 'pathname'
+require 'digest/sha1'
+
+module Yeb
+  class VirtualHost
+    attr_reader :hostname, :apps_dir, :sockets_dir
+
+    def initialize(hostname, apps_dir, sockets_dir)
+      @hostname = hostname
+      @apps_dir = apps_dir
+      @sockets_dir = sockets_dir
+    end
+
+    def app_symlink_path
+      symlinked_app_dir = "#{apps_dir}/#{hostname.app_name}"
+
+      if File.symlink?(symlinked_app_dir)
+        return symlinked_app_dir
+      end
+
+      if hostname != hostname.root
+        symlinked_app_dir = "#{apps_dir}/#{hostname.root.app_name}"
+
+        if File.symlink?(symlinked_app_dir)
+          return symlinked_app_dir
+        end
+      end
+
+      nil
+    end
+
+    def app_real_path
+      if path = app_symlink_path
+        Pathname.new(path).realpath.to_s # follow all symlinks
+      end
+    end
+
+    def app_name
+      if path = app_symlink_path
+        File.basename(path)
+      end
+    end
+
+    def socket_path
+      "#{sockets_dir}/#{hostname}.sock"
+    end
+
+    def app_socket_path
+      if path = app_real_path
+        path_hash = Digest::SHA1.hexdigest(path)
+        "#{sockets_dir}/#{path_hash}.sock"
+      end
+    end
+
+    def create_socket_symlink
+      if app_socket_path
+        if File.symlink?(socket_path) || File.exist?(socket_path)
+          File.unlink(socket_path)
+        end
+
+        File.symlink(app_socket_path, socket_path)
+      end
+    end
+
+    def app_symlinked?
+      !!app_symlink_path
+    end
+
+    def socket
+      raise AppNotFoundError.new(hostname.app_name) unless app_symlinked?
+
+      create_socket_symlink
+      spawn_app unless File.exist?(app_socket_path)
+
+      UNIXSocket.new(socket_path)
+    end
+
+    def spawn_app
+      app = App.new(app_name, app_real_path, app_socket_path)
+      app.spawn
+    end
+  end
+end
---
+++ spec/virtual_host_spec.rb
@@ -0,0 +1,159 @@
+require 'spec_helper'
+require 'tempfile'
+
+describe Yeb::VirtualHost do
+  let(:apps_dir) { Dir.mktmpdir }
+  let(:sockets_dir) { Dir.mktmpdir }
+  let(:hostname) { Yeb::Hostname.new('foo.dev') }
+  let(:vhost) { Yeb::VirtualHost.new(hostname, apps_dir, sockets_dir) }
+
+  def symlink_target(hostname)
+    "#{apps_dir}/#{hostname.name}".sub(/\.dev$/, '')
+  end
+
+  describe '#app_symlink_path' do
+    subject { vhost.app_symlink_path }
+
+    let(:hostname) { Yeb::Hostname.new('foo.bar.dev') }
+
+    context 'when app symlinked in apps_dir' do
+      before do
+        File.symlink('/tmp/app', symlink_target(hostname))
+      end
+
+      it { should == "#{apps_dir}/foo.bar" }
+    end
+
+    context 'when no app symlinked in apps_dir' do
+      it { should == nil }
+
+      context 'when root app symlinked in apps_dir' do
+        before do
+          File.symlink('/tmp/app', symlink_target(hostname.root))
+        end
+
+        it { should == "#{apps_dir}/bar" }
+      end
+    end
+  end
+
+  describe '#app_real_path' do
+    subject { vhost.app_real_path }
+
+    context 'when app_symlink_path is present' do
+      let(:real_path) do
+        file = Tempfile.new('real_path')
+        file.close
+        file.path
+      end
+
+      let(:double_symlink_path) do
+        File.symlink(real_path, "#{real_path}.1")
+        File.symlink("#{real_path}.1", "#{real_path}.2")
+        "#{real_path}.2"
+      end
+
+      before do
+        vhost.stub!(:app_symlink_path => double_symlink_path)
+      end
+
+      it { should == real_path }
+    end
+
+    context 'when app_symlink_path is not present' do
+      before do
+        vhost.stub!(:app_symlink_path => nil)
+      end
+
+      it { should be(nil) }
+    end
+  end
+
+  describe '#app_name' do
+    subject { vhost.app_name }
+
+    context 'when app_symlink_path is present' do
+      before do
+        vhost.stub!(:app_symlink_path => '/a/b/c')
+      end
+
+      it { should == 'c' }
+    end
+
+    context 'when app_symlink_path is not present' do
+      before do
+        vhost.stub!(:app_symlink_path => nil)
+      end
+
+      it { should be(nil) }
+    end
+  end
+
+  describe '#socket_path' do
+    subject { vhost.socket_path }
+
+    it { should == "#{sockets_dir}/#{hostname}.sock" }
+  end
+
+  describe '#app_socket_path' do
+    subject { vhost.app_socket_path }
+
+    context 'when app_real_path is present' do
+      before do
+        vhost.stub!(:app_real_path => '/a/b/c')
+      end
+
+      it { should =~ %r(^#{sockets_dir}/.+\.sock$) }
+    end
+
+    context 'when app_real_path is not present' do
+      before do
+        vhost.stub!(:app_real_path => nil)
+      end
+
+      it { should be(nil) }
+    end
+  end
+
+  describe '#create_socket_symlink' do
+    pending
+  end
+
+  describe '#app_symlinked?' do
+    subject { vhost.app_symlinked? }
+
+    context 'when app_symlink_path is present' do
+      before do
+        vhost.stub!(:app_symlink_path) { '/some' }
+      end
+
+      it { should be(true) }
+    end
+
+    context 'when app_symlink_path is not present' do
+      before do
+        vhost.stub!(:app_symlink_path) { nil }
+      end
+
+      it { should be(false) }
+    end
+  end
+
+  describe '#socket' do
+    pending
+  end
+
+  describe '#spawn_app' do
+    let(:app_name) { double('app_name') }
+    let(:dir) { double('dir') }
+    let(:socket_path) { double('socket_path') }
+    let(:app) { double('app') }
+
+    it 'creates new instance of App and calls #spawn on it' do
+      App.should_receive(:new).with(app_name, dir, socket_path).and_return(app)
+      app.should_receive(:spawn)
+
+      vhost.spawn
+    end
+  end
+end
