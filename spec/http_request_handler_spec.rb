c2e2f4bc6388eb109bf71962101f0e73b71f52aa
Marcin Kulik <marcin.kulik@gmail.com>
2012-12-17 00:25:42 +0100

    Spec for HTTPRequestHandler

--- spec/http_request_handler_spec.rb
+++ spec/http_request_handler_spec.rb
@@ -1,23 +1,32 @@
 require 'spec_helper'
 
 describe Yeb::HTTPRequestHandler do
-  let(:handler) { Yeb::HTTPRequestHandler.new }
+  let(:apps_dir) { Dir.mktmpdir }
+  let(:sockets_dir) { Dir.mktmpdir }
+  let(:handler) { Yeb::HTTPRequestHandler.new(apps_dir, sockets_dir) }
 
   describe '#get_response' do
-    let(:request_body) do
-      <<EOS
-GET / HTTP/1.1
-Accept: */*
-Accept-Encoding: gzip, deflate, compress
-Host: lulu.dev
-User-Agent: HTTPie/0.3.0
-EOS
+    let(:request) { double('request') }
+    let(:response) { double('response') }
+    let(:hostname) { double('hostname') }
+    let(:vhost) { double('vhost') }
+    let(:socket) { double('socket') }
+
+    subject { handler.get_response(request) }
+
+    before do
+      Yeb::Hostname.stub!(:from_http_request => hostname)
     end
 
-    subject { handler.get_response(request_body) }
-
-    it 'looks up app by hostname' do
-
+    it 'returns response from VirtualHost socket' do
+      # FIX: Violating law of Demeter like a boss
+      Yeb::VirtualHost.should_receive(:new).
+        with(hostname, apps_dir, sockets_dir).and_return(vhost)
+      vhost.should_receive(:socket).and_return(socket)
+      socket.should_receive(:send).with(request, 0)
+      socket.should_receive(:recv).and_return(response)
+      socket.should_receive(:close)
+      handler.get_response(request).should == response
     end
   end
 end
