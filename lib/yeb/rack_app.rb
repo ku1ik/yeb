78149f77eb7631ab1d5cb7460032d7b569f2a016
Marcin Kulik <marcin.kulik@gmail.com>
2012-12-18 00:08:39 +0100

    App -> RackApp

---
+++ lib/yeb/rack_app.rb
@@ -0,0 +1,55 @@
+module Yeb
+  class RackApp
+    @@apps = {}
+
+    attr_reader :name, :dir, :socket_path
+
+    def initialize(name, dir, socket_path)
+      @name = name
+      @dir = dir
+      @socket_path = socket_path
+      @@apps[name] = self # need to prevent garbage collection and closing of @stdout and @stderr
+    end
+
+    def spawn
+      puts "spawning app #{name} in #{dir}"
+
+      @process = Process.new(command)
+      @process.start
+
+      while @process.alive? && !File.exist?(socket_path)
+        puts "waiting for #{socket_path}"
+        sleep 1
+      end
+
+      unless File.exist?(socket_path)
+        raise AppStartFailedError.new(name, @process.stdout, @process.stderr, env)
+      end
+    end
+
+    def command
+      if File.exist?("#{dir}/config.ru")
+        Command.new("#{thin} start -S #{socket_path}", dir)
+      else
+        raise AppNotRecognizedError.new(name)
+      end
+    end
+
+    def thin
+      path = `which thin`.strip
+
+      if path == ''
+        raise 'nie ma thina'
+      end
+
+      path
+    end
+
+    def env
+      command = Command.new("/usr/bin/env | sort")
+      process = Process.new(command)
+      process.start
+      process.stdout + process.stderr
+    end
+  end
+end
